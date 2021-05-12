68866becc2c8e435e3673a326b3d7f6cc99bae9f
KamiMoon <erickizaki@gmail.com>
2021-05-11 20:27:30 -0500

    mock service worker

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,27 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server.js";
+
+//https://hceris.com/jest-fail-test-if-unexpected-network-request-happens/
+
+// const spies = {
+//   fetch: jest.spyOn(window, "fetch"),
+// };
+
+// beforeEach(() => {
+//   jest.resetAllMocks();
+// });
+
+// afterEach(() => {
+//   expect(spies.fetch).not.toHaveBeenCalled();
+// });
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
