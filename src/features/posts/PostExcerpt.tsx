7798e00a275a0d332e256b882e41c24912f31b42
KamiMoon <erickizaki@gmail.com>
2021-05-13 20:57:47 -0500

    post list display

--- src/features/posts/PostExcerpt.tsx
+++ src/features/posts/PostExcerpt.tsx
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { Card } from "antd";
+
 import { PostAuthor } from "./PostAuthor";
 import { TimeAgo } from "./TimeAgo";
 import { ReactionButtons } from "./ReactionButtons";
@@ -9,17 +11,21 @@ import { selectPostById } from "../../redux/slicers/postsSlice";
 export const PostExcerpt = ({ postId }) => {
   const post = useSelector((state) => selectPostById(state, postId));
   return (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <PostAuthor userId={post.user} />
-      <TimeAgo timestamp={post.date} />
+    <Card
+      title={
+        <Link to={`/posts/view/${post.id}`} className="button muted-button">
+          {post.title}
+        </Link>
+      }
+      style={{ marginBottom: "20px" }}
+    >
+      <article className="post-excerpt" key={post.id}>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
 
-      <ReactionButtons post={post} />
-
-      <Link to={`/posts/view/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
+        <ReactionButtons post={post} />
+      </article>
+    </Card>
   );
 };
