5e09b7ac56b0dcd31eec3221644f930adab9883e
KamiMoon <erickizaki@gmail.com>
2021-05-13 22:11:16 -0500

    fix navbar selection

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,36 @@
 import { Layout, Menu } from "antd";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const { Header } = Layout;
 
 export default function Navbar() {
+  let defaultSelectedKeys = ["posts"];
+
+  let location = useLocation();
+
+  if (location.pathname.includes("users")) {
+    defaultSelectedKeys = ["users"];
+  } else if (location.pathname.includes("login")) {
+    defaultSelectedKeys = ["login"];
+  }
+
   return (
     <Header>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-        <Menu.Item key="1">
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={defaultSelectedKeys}
+        selectedKeys={defaultSelectedKeys}
+      >
+        <Menu.Item key="posts">
           <Link to="/">Posts</Link>
         </Menu.Item>
-        <Menu.Item key="3">
+        <Menu.Item key="users">
           <Link to="/users">Users</Link>
         </Menu.Item>
-        <Menu.Item key="2">
+        <Menu.Item key="login">
           <Link to="/login">Login</Link>
         </Menu.Item>
       </Menu>
